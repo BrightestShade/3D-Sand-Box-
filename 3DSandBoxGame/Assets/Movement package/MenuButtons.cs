1597d270280473e60476ae490d1cde75e32e3dd7
oneVoltz <yasminzambaldecosta5@gmail.com>
2025-12-18 15:47:22 +0000

    menu

---
+++ 3DSandBoxGame/Assets/Movement package/MenuButtons.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    // PLAY BUTTON → loads scene index 0
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // QUIT BUTTON
+    public void QuitGame()
+    {
+        Application.Quit();
+
+        // Allows quit to work in the Unity Editor
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
