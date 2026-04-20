140b248f7df453e5799d11fe8ecb193b13cf39d4
BrightestShade <raphaelwood9@gmail.com>
2026-04-20 14:59:42 +0100

    Added some effects and gun stuff

---
+++ 3DSandBoxGame/Assets/ModularGunScript.cs
@@ -0,0 +1,115 @@
+using UnityEngine;
+using TMPro;
+public class ModularGunScript : MonoBehaviour
+{
+    //Gun stats
+    public int damage;
+    public float timeBetweenShooting, spread, range, reloadTime, timeBetweenShots;
+    public int magazineCapacity, bulletsPerTap;
+    public bool allowButtonHold;
+    int bulletsLeft, bulletsShot;
+
+    //bools
+    bool shooting, readyToShoot, reloading;
+
+    //reference
+    public Camera fpsCam;
+    public Transform attackPoint;
+    public RaycastHit rayHit;
+    public LayerMask whatIsEnemy;
+
+    // Graphics
+    public GameObject muzzleFlash;
+
+    // public CamShake camShake;
+    //public float camShakeMagnitude;
+    TextMeshProUGUI text;
+
+    private void Awake()
+    {
+        bulletsLeft = magazineCapacity;
+        readyToShoot = true;
+
+        //set the ammo count
+        text.SetText(bulletsLeft + " / " + magazineCapacity);
+    }
+
+
+
+    private void Update()
+    {
+        MyInput();
+    }
+
+
+
+    private void MyInput()
+    {
+        if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
+        else shooting = Input.GetKeyDown(KeyCode.Mouse0);
+
+        if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineCapacity && !reloading) Reload();
+        
+        //shoot
+        if (readyToShoot && shooting && !reloading && bulletsLeft > 0)
+        {
+            Shoot();
+        }
+    }
+    // shake the cam
+    // camShake.Shake(camShakeDuration, camShakeMagnitude);
+   
+
+
+
+  
+    private void Shoot()
+    {
+        readyToShoot = false;
+
+        //spread 
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+
+        // Calculate projectile direction with spread
+        Vector3 direction = fpsCam .transform.forward + new Vector3(x, y, 0);
+
+        // Raycast
+        if (Physics.Raycast(fpsCam.transform.position, direction, out rayHit, range, whatIsEnemy))
+        {
+
+            Debug.Log(rayHit.collider.name);
+
+            if (rayHit.collider.CompareTag("Enemy"))
+            {
+                // rayHit.collider.GetComponent<EnemyAi>().TakeDamage
+                Debug.Log("Enemy hit");
+            }
+
+        }
+
+        
+        Instantiate(muzzleFlash, attackPoint.position, Quaternion.identity);
+
+      bulletsLeft--;
+
+        Invoke("ResetShoot", timeBetweenShooting);
+    }
+
+    private void ResetShoot()
+    {
+        readyToShoot = true;
+    }
+    private void Reload()
+    {
+        reloading = true;
+        Invoke("ReloadFinished", reloadTime);
+
+    }
+
+    private void ReloadFinished()
+    {
+        bulletsLeft = magazineCapacity;
+        reloading = false;
+    }
+}
